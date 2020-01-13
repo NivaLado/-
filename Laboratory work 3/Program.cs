7b997d192e2c1921c8773438efb39e3995e0e38b
Mark Belaev <mark080897@gmail.com>
2020-01-13 19:54:07 +0200

    Lab 3

---
+++ Laboratory work 3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lab_3_NonLinearEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            double PreviousX;
+            int iterations = 0;
+
+            Console.WriteLine("Enter initial approximation");
+            input = Console.ReadLine();
+
+            double CurrentX = Convert.ToInt32(input);
+
+            Console.WriteLine($"X0 = {CurrentX}");
+            double dX = double.MaxValue;
+
+            while (Math.Abs(dX) > 1e-4)
+            {
+                PreviousX = CurrentX;
+                CurrentX = F2(PreviousX);
+                dX = CurrentX - PreviousX;
+                iterations++;
+            }
+
+            Console.WriteLine($"Iterations = {iterations}");
+            Console.WriteLine($"X = {CurrentX}");
+            Console.ReadLine();
+        }
+
+        static double F (double x)
+        {
+            return (x * x + 4) / 5;
+        }
+
+
+        // X * e^X/2 = 4
+        // Приводим к x
+        static double F2 (double x)
+        {
+            return 4 / (Math.Pow(Math.E, x / 2));
+        }
+    }
+}
