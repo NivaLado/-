64135409f707f3e7ff0d9e027ceead1c5e5b3843
Mark Belaev <mark080897@gmail.com>
2020-01-23 22:57:45 +0200

    Lab - 4

---
+++ Laboratory work 4/EulerMethod.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EulerMethod
+{
+    public class EulerMethod
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public List<double> XList { get; set; }
+
+        public List<double> YList { get; set; }
+
+        public EulerMethod()
+        {
+            XList = new List<double>();
+            YList = new List<double>();
+        }
+
+        public (List<double>, List<double>) Solve(double startXPoint, double endXPoint, double initialСondition, double step)
+        {
+            int overalSteps = (int)((endXPoint - startXPoint) / step);
+
+            X = startXPoint;
+            Y = initialСondition;
+
+            XList.Add(X);
+            YList.Add(Y);
+
+            for (int i = 0; i < overalSteps; i++)
+            {
+                Y += step * F(X, Y);
+                YList.Add(Y);
+
+                X += step;
+                XList.Add(X);
+            }
+
+            return (XList, YList);
+        }
+
+        private double F(double x, double y)
+        {
+            return (-3 * y) + (2 * Math.Pow(x,2));
+        }
+    }
+}
---
+++ Laboratory work 4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EulerMethod
+{
+
+    struct XYCoordinates
+    {
+        public List<double> X, Y;
+
+        public int Length;
+
+        public XYCoordinates(List<double> x,List<double> y)
+        {
+            X = x;
+            Y = y;
+
+            Length = x.Count;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var euler = new EulerMethod();
+
+            var result = euler.Solve(0, 1, 13, 0.01f);
+
+            XYCoordinates coordinates = new XYCoordinates(result.Item1, result.Item2);
+
+            for (int i = 0; i < coordinates.Length; i++)
+            {
+                Console.WriteLine($"X is : {coordinates.X[i]}, Y is {coordinates.Y[i]}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
